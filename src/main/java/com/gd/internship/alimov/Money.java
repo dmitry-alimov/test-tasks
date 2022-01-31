eafe4820ea45daacf53afafac877a7868e44a946
Dmitry Alimov <dalimov@griddynamics.com>
2022-01-31 10:09:26 +0400

    design immutable class

---
+++ src/main/java/com/gd/internship/alimov/Money.java
@@ -0,0 +1,41 @@
+package com.gd.internship.alimov;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
+public class Money {
+
+    private final BigDecimal amount;
+    private final Currency currency;
+
+    public Money(BigDecimal amount, Currency currency) {
+        this.amount = amount;
+        this.currency = currency;
+    }
+
+    public Money put(BigDecimal plus) {
+        return new Money(amount.add(plus).setScale(2, RoundingMode.HALF_UP), currency);
+    }
+
+    public Money take(BigDecimal taken) {
+        return new Money(amount.subtract(taken).setScale(2, RoundingMode.HALF_UP), currency);
+    }
+
+    public BigDecimal getAmount() {
+        return amount;
+    }
+
+    public Currency getCurrency() {
+        return currency;
+    }
+
+    @Override
+    public String toString() {
+        return "Money{" +
+                "amount=" + amount +
+                ", currency='" + currency + '\'' +
+                '}';
+    }
+
+    public enum Currency {USD, RUB, EUR, JPY, GBP}
+}
